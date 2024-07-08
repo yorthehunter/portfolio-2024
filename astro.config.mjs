cae7293d67a83e96abb12f9b1d249933e250a99f
Brian Cleveland <brian.cleveland@getbuilt.com>
2024-07-08 16:53:21 -0400

    astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,7 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    site:'https://yorthehunter.github.io',
+    base: 'portfolio-2024',
+});
